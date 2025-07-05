bc4bfab8e0e9b00f4d026993a1b5ce98e95b724f
KhushiSaini2321 <khushisaini5911@gmail.com>
2025-07-05 11:06:22 +0530

    variablles
    oo

---
+++ variables.py
@@ -0,0 +1,15 @@
+print("------------------------variables-----------------------------")
+print("hello world","my name is khushi")#first program
+print(34)
+print(23)
+print(3+6)
+#varibles
+name = "khushi"
+age = 23
+price = 25.66
+age = 9
+print (name,"\n",age,"\n", price,"\n","of a person")#\n for next line
+#type of variable
+print(type(name))
+print(type(age))
+print(type(price))
